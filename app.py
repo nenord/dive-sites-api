8d6c8320990335a4f2ca82fe17fb14d95a86f2f8
nenord <63916787+nenord@users.noreply.github.com>
2021-02-13 18:41:07 +0000

    Update and rename app/main.py to app.py

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ async def read_site(site_slug: str, db: Session = Depends(get_db)):
     raise HTTPException(status_code=404, detail="Site not found")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app)
