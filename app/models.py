70c0cbacbc221bd8140169270d17decdd8bc24db
nenord <63916787+nenord@users.noreply.github.com>
2021-02-13 17:59:31 +0000

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, Float
 
-from app.database import Base
+from database import Base
 
 class Site_db(Base):
     __tablename__ = "sites"
