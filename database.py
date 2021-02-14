49cb1cd0812120f736dea3f5f38761c19600ca13
nenord <63916787+nenord@users.noreply.github.com>
2021-02-14 17:20:47 +0000

    Remove Azure db - add heroku db
    Azure db string did not work, porting this app to Heroku

--- database.py
+++ database.py
@@ -1,13 +1,10 @@
 import os
-import pyodbc
-import urllib
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-params = urllib.parse.quote_plus(r'Driver={ODBC Driver 13 for SQL Server};Server=tcp:dive-sites.database.windows.net,1433;Database=sites;Uid=nenord;Pwd=<password>;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
-engine = create_engine(conn_str,echo=True)
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
