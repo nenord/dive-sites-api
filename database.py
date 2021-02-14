017015dbe5ade341f292cf19855ffac776985044
nenord <63916787+nenord@users.noreply.github.com>
2021-02-14 12:07:06 +0000

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,14 @@
 import os
+import pyodbc
+import urllib
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-username = os.environ.get('USER')
-password = os.environ.get('PASSWORD')
-server = os.environ.get('SERVER')
-db = os.environ.get('DATABASE')
-
-SQLALCHEMY_DATABASE_URL = f'mysql+mysqlconnector://{username}:{password}@{server}/{db}'
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+params = urllib.parse.quote_plus \ # urllib.parse.quote_plus for python 3
+(r'Driver={ODBC Driver 13 for SQL Server};Server=tcp:dive-sites.database.windows.net,1433;Database=sites;Uid=nenord;Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+engine = create_engine(conn_str,echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
