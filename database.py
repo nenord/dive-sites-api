8fa02697f73b93978509db760a1bd946afa6b098
nenord <63916787+nenord@users.noreply.github.com>
2021-02-14 12:13:40 +0000

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-params = urllib.parse.quote_plus \ # urllib.parse.quote_plus for python 3
+params = urllib.parse.quote_plus \
 (r'Driver={ODBC Driver 13 for SQL Server};Server=tcp:dive-sites.database.windows.net,1433;Database=sites;Uid=nenord;Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
 conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
 engine = create_engine(conn_str,echo=True)
